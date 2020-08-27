30ceedbb23e386745df186a7a865728b69002cce
Brendan-Rogers <brendanr11@gmail.com>
2020-08-26 23:10:13 -0400

    Added Boolean Question support. NEXT - Display SCORE

--- js/index.js
+++ js/index.js
@@ -35,23 +35,44 @@
 		headlineOut.innerHTML = `${question.category.toUpperCase()} - ${question.difficulty.toUpperCase()}`;
 		questionOut.innerHTML = question.question;
 
-		// set Correct Question
-		let correct_location = Math.floor(Math.random() * Math.floor(4));
-		btn[correct_location].innerHTML = question.correct_answer;
-		btn[correct_location].value = 1;
+		// Load the ANSWERS into BTNs
+		switch (question.type) {
+			// Load MULTIPLE CHOICE question
+			case 'multiple':
+				// make Buttons 3 and 4 visible, after TRUE / FALSE question
+				btn[2].style.visibility = "visible";
+				btn[3].style.visibility = "visible";
+				// set Correct Question
+				let correct_location = Math.floor(Math.random() * Math.floor(4));
+				btn[correct_location].innerHTML = question.correct_answer;
+				btn[correct_location].value = 1;
 
-		// Iterator for Incorrect Questions (there are 3)
-		x = 0;
-		// Loop through BUTTONS
-		for (i = 0; i < 4; i++) {
-			// escape if we're on Correct Answer
-			if (i == correct_location) { continue; }
-			// button needs a question, so we write the first Wrong Answer to it
-			btn[i].innerHTML = question.incorrect_answers[x];
-			btn[i].value = 0;
-			// increment X, so the next time we have a button to fill, we get the next incorrect answer
-			x++;
+				// Iterator for Incorrect Questions (there are 3)
+				x = 0;
+				// Loop through BUTTONS
+				for (i = 0; i < 4; i++) {
+					// escape if we're on Correct Answer
+					if (i == correct_location) { continue; }
+					// button needs a question, so we write the first Wrong Answer to it
+					btn[i].innerHTML = question.incorrect_answers[x];
+					btn[i].value = 0;
+					// increment X, so the next time we have a button to fill, we get the next incorrect answer
+					x++;
+				}
+				break;
+			// Load TRUE / FALSE question
+			case 'boolean':
+				// load TRUE / FALSE buttons
+				btn[0].innerHTML = "True";
+				btn[1].innerHTML = "False";
+				btn[2].style.visibility = "hidden";
+				btn[3].style.visibility = "hidden";
+				
+
+				break;
 		}
+
+		
 	}
 	// Creates API URL on state change
 	function constructURL(category, difficulty) {
@@ -63,9 +84,6 @@
 
 	// call default trivia
 	getTrivia(url);
-
-
-
 
 	// EVENT HANDLING
 	btn.forEach((element) => {
